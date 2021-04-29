3a402acba961e4850267bbfc727cb055b58d69f5
Laís Cavalcanti <cavalcantislais@gmail.com>
2021-04-29 01:00:18 -0300

    falta finalizar teste de link

---
+++ src/components/LinkWrapper/test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import LinkWrapper from '.'
+
+describe('<LinkWrapper />', () => {
+    it('should render link and children', () => {
+        render()
+    })
+})
