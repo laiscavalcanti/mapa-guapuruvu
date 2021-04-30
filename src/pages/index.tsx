890202220b52c4754e48ea3dc71e6b6cd60a4ee6
Laís Cavalcanti <cavalcantislais@gmail.com>
2021-04-30 01:17:03 -0300

    component link wrapper e page about feita

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import Main from 'components/Main'
 import dynamic from 'next/dynamic'
 
+import { InfoOutline } from '@styled-icons/evaicons-outline'
+import LinkWrapper from 'components/LinkWrapper'
+
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
@@ -8,6 +11,9 @@ export default function Home() {
   return (
     <>
       <Main />
+      <LinkWrapper href="/about"  >
+        <InfoOutline size={32} aria-label="About" />
+      </LinkWrapper>
       <Map />
     </>
 
