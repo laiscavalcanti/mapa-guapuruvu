da42dc4645466ff5b7ee445ec059d50540eb447a
Lais Cavalcanti <cavalcantislais@gmail.com>
2021-05-04 01:18:16 -0300

    conteudo dinamico

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,14 +1,25 @@
-import {gql} from 'graphql-request'
+import { gql } from 'graphql-request'
 
 export const GET_PAGES = gql`
-    query getPages{
-        pages{
-            id
-            heading
-            slug
-            body {
-                html
-            }
-        }
+  query getPages($first: Int) {
+    pages(first: $first) {
+      id
+      slug
+      body {
+        html
+      }
     }
+  }
+`
+export const GET_PAGE_BY_SLUG = gql`
+  query getPageBySlug($slug: String) {
+    page(where: { slug: $slug }) {
+      id
+      slug
+      heading
+      body {
+        html
+      }
+    }
+  }
 `
---
+++ src/pages/[slug].tsx
@@ -0,0 +1,42 @@
+import client from 'graphql/Client'
+import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries'
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/dist/client/router'
+//import AboutTemplate from 'templates/About'
+import PageTemplate, { PageTemplateProps } from 'templates/Pages'
+
+export default function Page({ heading, body}: PageTemplateProps) {
+  const router = useRouter()
+
+    // para gerar as demais páginas que não são as 3 primeiras definidas no 
+  //getStaticPaths
+
+  if(router.isFallback) return null //podendo terorna um loading, por ex, enquanto carrega a página
+
+  return <PageTemplate heading={heading} body={body}/>
+}
+
+
+export async function getStaticPaths(){ // criando o caminho das páginas
+    const { pages } = await client.request(GET_PAGES, {first: 3}) //fazendo a requisição para as 3 primeiras pages
+
+    const paths = pages.map(({ slug }) => ({ // o map para buscar as páginas
+        params: { slug }
+    }))
+    return { paths, fallback: true}
+}
+
+export const getStaticProps: GetStaticProps = async ({ params}) => {
+  const {page} = await client.request(GET_PAGE_BY_SLUG, {
+    slug: `${params?.slug}`
+  })
+
+  if(!page) return {notFound: true}
+
+  return {
+    props: {
+      heading: page.heading,
+      body: page.body.html
+    }
+  }
+}
