e557307ac75bac24299507729414e68af20eedcd
Lais Cavalcanti <cavalcantislais@gmail.com>
2021-05-03 23:17:14 -0300

    graphql client feito

---
+++ src/graphql/Client.ts
@@ -0,0 +1,11 @@
+import { GraphQLClient } from 'graphql-request'
+
+const endpoint = process.env.GRAPHQL_HOST || ' '
+
+const Client = new GraphQLClient(endpoint, {
+    headers: {
+        authorization: `Bearer ${process.env.GRAPHQL_TOKEN}`
+    }
+})
+
+export default Client
