fee99f0cbd0db56ad03002d5cd62cd29efc62a25
Lais Cavalcanti <cavalcantislais@gmail.com>
2021-05-13 01:19:02 -0300

    componente next iamge feito

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 import * as S from './style'
 import NavBar from 'components/NavBar'
 import Burger from 'components/Burger'
+import ImageWrapper from 'components/ImageWrapper'
 
 const Header = () => {
   const [isOpen, setOpen] = React.useState(false) // state para componente fechar e abrir
   return (
     <S.HeaderWrapper>
-      <h1>Mapa de Guapuruvus</h1>
+      <ImageWrapper
+        src="/logo.png"
+        alt="logo que remete a árvore guapuruvu"
+        width={500}
+        height={500}
+      />
       <Burger isOpen={isOpen} setOpen={setOpen} />
       <NavBar isOpen={isOpen} setOpen={setOpen} />
     </S.HeaderWrapper>
---
+++ src/components/ImageWrapper/index.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+export type imageWrapperProps = {
+    src: string,
+    alt: string,
+    width: number,
+    height: number
+}
+
+const ImageWrapper = ({ src, alt, width, height}: imageWrapperProps ) =>{
+    return(
+        <Image
+            src={src}
+            alt={alt}
+            width={width}
+            height={height}
+        />
+    )
+}
+
+export default ImageWrapper
