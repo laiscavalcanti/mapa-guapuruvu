ca41524ac5f78699b467a5d23dabaf65b876f67c
Lais Cavalcanti <cavalcantislais@gmail.com>
2021-06-11 01:01:28 -0300

    section animation done

--- src/components/Iconographic/index.tsx
+++ src/components/Iconographic/index.tsx
@@ -8,28 +8,53 @@ import takePhoto from './animations/take-photo.json'
 const Icongraphic = () => {
   return (
     <S.IcongraphicWrapper>
+      <S.Number>1</S.Number>
+      <S.TextArea>
+        <S.Title>encontre um Guapuruvu</S.Title>
+        <S.Text>
+          anote o endereço ou a localização do Guapuruvu que você encontrou
+        </S.Text>
+      </S.TextArea>
       <S.Icon>
         <Lottie
-          width="100px"
-          height="100px"
+          width="150px"
+          height="150px"
           className="lottie-container basic"
           config={{ animationData: mapView, loop: true, autoplay: true }}
         />
       </S.Icon>
 
+      <S.Line />
+      <S.Number>2</S.Number>
+      <S.TextArea>
+        <S.Title>tire uma foto</S.Title>
+        <S.Text>
+          registre através de fotos o Guapuruvu que você encontrou
+        </S.Text>
+      </S.TextArea>
       <S.Icon>
-      <Lottie
-          width="100px"
-          height="100px"
+        <Lottie
+          width="150px"
+          height="150px"
           className="lottie-container basic"
           config={{ animationData: photo, loop: true, autoplay: true }}
         />
       </S.Icon>
 
+      <S.Line />
+      <S.TextArea>
+      <S.Number>3</S.Number>
+        <S.Title>nos envie as informações</S.Title>
+        <S.Text>
+          tamanho, colocaração, idade - essas informações serão enviadas através
+          desse formulário
+        </S.Text>
+      </S.TextArea>
+
       <S.Icon>
-      <Lottie
-          width="100px"
-          height="100px"
+        <Lottie
+          width="150px"
+          height="150px"
           className="lottie-container basic"
           config={{ animationData: email, loop: true, autoplay: true }}
         />
@@ -37,4 +62,6 @@ const Icongraphic = () => {
     </S.IcongraphicWrapper>
   )
 }
+
+
 export default Icongraphic
--- src/components/Iconographic/style.tsx
+++ src/components/Iconographic/style.tsx
@@ -1,12 +1,41 @@
-import styled from "styled-components"
+import styled from 'styled-components'
 
 export const IcongraphicWrapper = styled.div`
-    width: 100%;
-    height: 20rem;
-    display: flex;
-    background-color: var(--background);
+  width: 100%;
+  height: 40rem;
+  display: flex;
+  justify-content: center;
+  background-color: var(--background);
+  padding: 4rem;
+  border: 1px solid white;
+`
+export const Line = styled.span`
+  margin: .5rem 0 .5rem 0;
+  border-left: solid 1px #222;
+  padding-left: 2rem;
+`
+export const Icon = styled.section`
+  display: flex;
+  justify-content: center;
+  margin: 0 .5rem 0 .5rem;
+`
+export const TextArea = styled.div`
+  max-width: 20%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `
 
-export const Icon = styled.section`
-    border-left: solid 1px #222;
+export const Number = styled.p`
+  display: flex;
+  flex-direction: column;
+  font-size: 1.5rem;
+`
+export const Title = styled.h2`
+  font-size: var(--small);
+  margin-bottom: 1rem;
+`
+export const Text = styled.p`
+  font-size: var(--smallest);
+  max-width: 80%;
 `
