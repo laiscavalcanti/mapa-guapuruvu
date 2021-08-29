7afe9555001c44175d84c4c1c7e77080dcdd2cd3
laís sousa <cavalcantislais@gmail.com>
2021-08-28 23:17:57 -0300

    page places in responsive mode

--- src/templates/Places/style.ts
+++ src/templates/Places/style.ts
@@ -1,10 +1,15 @@
 import styled from 'styled-components'
+import media from 'styled-media-query'
 
 export const Wrapper = styled.div`
   display: flex;
   justify-content: flex-start;
-  width: 90%;
+  width: 80%;
   padding: 8rem;
+  ${media.lessThan('medium')`
+    padding:3rem;
+    width: 95%;
+  `}
 `
 export const Container = styled.section`
   display: flex;
@@ -14,6 +19,10 @@ export const Container = styled.section`
   width: 75%;
   background-color: rgba(139, 174, 99, 0.37);
   padding: 4rem 8rem 4rem 8rem;
+  ${media.lessThan('medium')`
+    padding: 2rem 1rem 2rem 1rem;
+    width: 90%;
+  `}
 `
 export const ButtonShareWrapper = styled.div`
   display: flex;
@@ -21,6 +30,9 @@ export const ButtonShareWrapper = styled.div`
   position: absolute;
   left: 90rem;
   top: 10rem;
+  ${media.lessThan('medium')`
+    left: 36rem;
+  `}
 `
 export const IconShare = styled.li``
 
@@ -29,6 +41,9 @@ export const Heading = styled.h1`
   margin-bottom: var(--medium);
   text-align: center;
   font-weight: bold;
+  ${media.lessThan('medium')`
+    font-size: var(--small);
+  `}
 `
 export const ListField = styled.div`
   margin: 1rem 2rem 4rem 0;
@@ -51,6 +66,13 @@ export const Body = styled.div`
     margin-bottom: var(--medium);
     font-size: var(--small);
   }
+
+  ${media.lessThan('medium')`
+  p {
+    margin-bottom: var(--medium);
+    font-size: var(--smallest);
+  }
+  `}
 `
 export const Gallery = styled.div`
   display: grid;
